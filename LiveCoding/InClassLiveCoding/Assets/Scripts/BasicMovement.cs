a7a57036fb7b1189fb802d1355ae44d7dcd9f0eb
Aaron Freedman <aaron@secretcrush.net>
2017-09-06 11:43:31 -0400

    Cleaned up and added note to movement script

--- LiveCoding/InClassLiveCoding/Assets/Scripts/BasicMovement.cs
+++ LiveCoding/InClassLiveCoding/Assets/Scripts/BasicMovement.cs
@@ -1,50 +1,56 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BasicMovement : MonoBehaviour
 {
-	public Rigidbody2D MyRigidbody2D;
-	public float ForceStrength;
+    public Rigidbody2D MyRigidbody2D;
+    public float ForceStrength;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
+    // Use this for initialization
+    void Start()
+    {
+    }
 
-		if (Input.GetAxis("Vertical") > 0)
-		{
-			Debug.Log("this button moves me up");
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Moving GameObjects:
+        // You can use the Transform component always
+        // if it's also a physics object you can use the Rigidbody/Rigidbody2D
+        // If you move a physics object's transform, it will ignore physics interactions during the movement
+        // Make sure you use the rigidbody to move an object if you want it to have physics interactions during the
+        // movement. Make sure not to move it too quickly.
+        
+        // Input:
+        // Use Input.GetKey() to get a physical key on your keyboard
+        // Use Input.GetButton() or Input.GetAxix() to use your buttons/axes defined in the InputManager
+        // Remember there's also Input.GetButtonDown() and Input.GetButtonUp()
+        
+        // move up
+        if (Input.GetAxis("Vertical") > 0)
+        {
+            Debug.Log("this button moves me up");
+            
 //			transform.Translate(new Vector3(0, 1, 0));
 //			transform.Translate(Vector2.up * 5);
-			MyRigidbody2D.AddForce(Vector2.up * ForceStrength);
-			// move up
-		}
-		if (Input.GetAxis("Vertical") < 0)
-		{
-			// move down
-			
+            MyRigidbody2D.AddForce(Vector2.up * ForceStrength);
+        }
+        // move down
+        if (Input.GetAxis("Vertical") < 0)
+        {
 //			transform.Translate(Vector2.down * 5);
 //			MyRigidbody2D.MovePosition(Vector2.down * ForceStrength);
-			MyRigidbody2D.AddForce(Vector2.down * ForceStrength);
-
-		}
-		if (Input.GetAxis("Horizontal") < 0)
-		{
-			// move left
-			MyRigidbody2D.AddForce(Vector2.left * ForceStrength);
-
-		}
-		if (Input.GetAxis("Horizontal") > 0)
-		{
-			// move right
-			MyRigidbody2D.AddForce(Vector2.right * ForceStrength);
-
-		}
-		
-	}
+            MyRigidbody2D.AddForce(Vector2.down * ForceStrength);
+        }
+        // move left
+        if (Input.GetAxis("Horizontal") < 0)
+        {
+            MyRigidbody2D.AddForce(Vector2.left * ForceStrength);
+        }
+        // move right
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            MyRigidbody2D.AddForce(Vector2.right * ForceStrength);
+        }
+    }
 }
